be3b5b9a3c2896fcbb8345927c7770241a6ae0ff
GrommuL <miketsvg@gmail.com>
2023-07-16 12:26:44 +0600

    feat: added BestFreelancerSection screen

---
+++ src/components/screens/best-freelancers-section/BestFreelancerSection.tsx
@@ -0,0 +1,27 @@
+import { PortfolioItem } from './ui'
+
+export const BestFreelancerSection = () => {
+	return (
+		<section className='mb-[100px] pb-[32px]'>
+			<div className='container'>
+				<div className='flex flex-col gap-[40px]'>
+					<h2 className='font-bold text-[48px] leading-[120%]'>
+						Наши лучшие исполнители
+					</h2>
+					<div className='flex items-center justify-center gap-[32px] flex-wrap'>
+						<PortfolioItem />
+						<PortfolioItem />
+						<PortfolioItem />
+						<PortfolioItem />
+						<PortfolioItem />
+						<PortfolioItem />
+						<PortfolioItem />
+						<PortfolioItem />
+						<PortfolioItem />
+						<PortfolioItem />
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
