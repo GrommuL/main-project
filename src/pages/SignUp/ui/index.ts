2383b0d0c972b732c09bc45affed1dcb7c262cb7
GrommuL <miketsvg@gmail.com>
2023-07-19 12:39:14 +0600

    feat: added export components from ui folder

---
+++ src/pages/SignUp/ui/index.ts
@@ -0,0 +1,3 @@
+export { AccountRoleCheckbox } from './AccountRoleCheckbox'
+export { AcceptTermsCheckbox } from './AcceptTermsCheckbox'
+export { PasswordRuleNotification } from './PasswordRuleNotification'
