347c8c03b24ac3839f4653dd4ce33a702731af2b
GrommuL <miketsvg@gmail.com>
2023-07-20 14:57:31 +0600

    fix: change type from RegisterFields to User

--- src/utils/hooks/useSignUp.ts
+++ src/utils/hooks/useSignUp.ts
@@ -1,20 +1,22 @@
-import { RegisterFields } from '@/types/RegisterFields'
+import { User, UserInfo } from '@/types/User'
 import { instance } from '../axios/instance'
 import axios from 'axios'
 
 export const useSignUp = () => {
-	const createUser = async (userData: RegisterFields) => {
+	const createUser = async (userData: User) => {
+		const currentDate = new Date()
 		const user = {
 			firstName: userData.firstName,
 			lastName: userData.lastName,
 			email: userData.email,
 			password: userData.password,
 			acceptTerms: userData.acceptTerms,
-			accountRole: userData.accountRole
+			accountRole: userData.accountRole,
+			createdAt: currentDate.getTime()
 		}
 
 		try {
-			await instance.post('users', user)
+			const { data }: UserInfo = await instance.post('users', user)
 		} catch (error) {
 			if (axios.isAxiosError(error)) {
 				console.log(error.response)
