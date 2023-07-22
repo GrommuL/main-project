f16cd99d263b92ea17da62d8f01fc869f95dfa89
GrommuL <miketsvg@gmail.com>
2023-07-22 12:50:34 +0600

    fix: added user existence check

--- src/utils/hooks/useSignIn.ts
+++ src/utils/hooks/useSignIn.ts
@@ -1,13 +1,20 @@
 import { User } from '@/types/User'
 import { instance } from '../axios/instance'
 import axios from 'axios'
+import { useState } from 'react'
 
 export const useSignIn = () => {
+	const [message, setMessage] = useState('')
 	const loginUser = async (userData: User) => {
 		try {
 			const response = await instance.post('login', userData)
 		} catch (error) {
 			if (axios.isAxiosError(error)) {
+				if (error.response?.status === 400) {
+					setMessage(
+						'Такого пользователя не существует, неправильный email или пароль'
+					)
+				}
 				console.log(error.response)
 			} else if (error instanceof Error) {
 				console.log(error.message)
@@ -15,6 +22,7 @@ export const useSignIn = () => {
 		}
 	}
 	return {
-		loginUser
+		loginUser,
+		message
 	}
 }
