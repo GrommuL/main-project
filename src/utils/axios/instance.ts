4b7decb7e961026102bdafb57952770648639db9
GrommuL <miketsvg@gmail.com>
2023-07-22 14:07:57 +0600

    fix: change headers in instance

--- src/utils/axios/instance.ts
+++ src/utils/axios/instance.ts
@@ -3,5 +3,15 @@ import axios from 'axios'
 export const instance = axios.create({
 	baseURL: import.meta.env.VITE_APP_BASE_URL as string,
 	timeout: 1000,
-	headers: { 'X-Custom-Header': 'foobar' }
+	headers: { 'Content-Type': 'application/json' }
 })
+
+// instance.interceptors.request.use(config =>{
+// 	const accessToken = Cookies.get('accesToken')
+
+// 	if(config.headers && accessToken){
+// 		config.headers.Authorization = `Bearer ${accessToken}`
+
+// 		return config
+// 	}
+// })
