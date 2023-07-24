592a78e657feea05f1930e88c16106d0fe9f3c6f
GrommuL <miketsvg@gmail.com>
2023-07-24 14:26:51 +0600

    feat: added the display of the button "create task" or "find task" depending on the role of the account

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,17 +1,21 @@
+import { useAppSelector } from '@/utils/hooks/redux'
 import { HeaderLinkButton, HeaderLogoLinkButton } from './ui'
 import { LinkButton } from '@/components/ui/buttons'
 
 export const Header = () => {
+	const accountRole = useAppSelector((state) => state.auth.user?.accountRole)
 	return (
 		<header className='shadow-header'>
 			<div className='container'>
 				<div className='flex items-center justify-between h-[80px]'>
 					<div className='flex items-center gap-[48px]'>
 						<HeaderLogoLinkButton />
-						<div className='flex items-center'>
-							<HeaderLinkButton label='Создать задание' href='/' />
+						{accountRole === 'freelancer' && (
 							<HeaderLinkButton label='Найти задание' href='/' />
-						</div>
+						)}
+						{accountRole === 'customer' && (
+							<HeaderLinkButton label='Создать задание' href='/' />
+						)}
 					</div>
 					<LinkButton label='Вход и регистрация' href='/login' />
 				</div>
