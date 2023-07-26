53eb7de3e15326042fa3bc7709471081ecfb64a7
GrommuL <miketsvg@gmail.com>
2023-07-26 14:20:18 +0600

    fix: remove all unused imports

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,8 @@
 import { Header } from './header'
 import { Footer } from './footer'
-import { Outlet, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-import Cookies from 'js-cookie'
-import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux'
-import { AuthService } from '@/services/auth'
-import { logoutUser } from '@/store/slices/authSlice'
+import { Outlet } from 'react-router-dom'
 
 export const Layout = () => {
-	const { removeTokenFromStorage } = AuthService()
-	const pathname = useLocation()
-	const dispatch = useAppDispatch()
-	const user = useAppSelector((state) => state.auth.user)
-
 	return (
 		<>
 			<Header />
