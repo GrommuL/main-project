4e090fb4909efebeb686201e2997f5c10fa3a4d7
GrommuL <miketsvg@gmail.com>
2023-08-01 14:51:41 +0600

    feat: added ProfileAboutEdit component

---
+++ src/pages/Profile/ui/components/ProfileAboutEdit.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/buttons'
+import { Heading } from '@/components/ui/heading'
+
+export const ProfileAboutEdit = () => {
+	return (
+		<form className='flex flex-col gap-[24px] items-center justify-center'>
+			<Heading
+				title='О себе'
+				description='Расскажите о себе. Какими навыками обладаете, чем любите заниматься?'
+			/>
+			<textarea
+				className='border border-borderColor rounded-[16px] p-[12px] outline-none hover:border-primaryHover focus:border-primary'
+				name='about'
+				id='about'
+				cols={60}
+				rows={10}
+				placeholder='Введите текст'
+			/>
+			<Button label='Отправить' className='w-[200px]' />
+		</form>
+	)
+}
