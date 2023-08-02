b310e2ce447902c7e76938f4fc219c01c82704a6
GrommuL <miketsvg@gmail.com>
2023-08-02 14:43:31 +0600

    update: added ProfileFreelancerPortfolio display if accountRole is freelancer

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,12 +1,19 @@
-import { ProfileGeneralSettings, ProfileUserCard } from './ui'
+import { useAppSelector } from '@/utils/hooks/redux'
+import {
+	ProfileFreelancerPortfolio,
+	ProfileGeneralSettings,
+	ProfileUserCard
+} from './ui'
 
 export const Profile = () => {
+	const user = useAppSelector((state) => state.auth.user)
 	return (
 		<main className='pt-[50px]'>
 			<div className='container'>
 				<div className='flex flex-col gap-[50px]'>
 					<ProfileUserCard />
 					<ProfileGeneralSettings />
+					{user?.accountRole === 'freelancer' && <ProfileFreelancerPortfolio />}
 				</div>
 			</div>
 		</main>
