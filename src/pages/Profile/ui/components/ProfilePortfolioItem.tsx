9f73f220dba199d12e33322b346cad31aab4de2d
GrommuL <miketsvg@gmail.com>
2023-08-03 15:26:23 +0600

    update: added data rendering based on received props

--- src/pages/Profile/ui/components/ProfilePortfolioItem.tsx
+++ src/pages/Profile/ui/components/ProfilePortfolioItem.tsx
@@ -1,7 +1,15 @@
-import image from '/image.jpg'
 import { BriefcaseIcon, CloseIcon } from '@/components/ui/icons'
+import { FC } from 'react'
 
-export const ProfilePortfolioItem = () => {
+type ProfilePortfolioItemProps = {
+	label: string
+	image: string
+}
+
+export const ProfilePortfolioItem: FC<ProfilePortfolioItemProps> = ({
+	label,
+	image
+}) => {
 	return (
 		<div className='flex flex-col items-center max-w-[276px] w-full relative'>
 			<button className='absolute top-[16px] left-[16px] w-[44px] h-[44px] rounded-full bg-primary flex items-center justify-center hover:bg-primaryHover'>
@@ -11,12 +19,12 @@ export const ProfilePortfolioItem = () => {
 				<CloseIcon />
 			</button>
 			<img
-				className='w-[276px] h-[180px] rounded-t-[16px]'
+				className='w-[276px] h-[180px] rounded-t-[16px] object-cover'
 				src={image}
 				alt='Portfolio'
 			/>
 			<span className='text-[20px] font-semibold leading-[26px] py-[24px] px-[16px]'>
-				Дизайн сервиса туроператора I LIKE TRAVEL
+				{label}
 			</span>
 		</div>
 	)
