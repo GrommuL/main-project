0bc3756032b8254558ca8b3e5a8cbaa16879a6cf
GrommuL <miketsvg@gmail.com>
2023-08-05 13:48:28 +0600

    update: added variants for button

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -5,13 +5,25 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	className?: string
 	label: string
 	onClick?: () => void
+	variant?: 'primary' | 'outline'
 }
 
-export const Button: FC<ButtonProps> = ({ label, onClick, className }) => {
+export const Button: FC<ButtonProps> = ({
+	label,
+	onClick,
+	className,
+	variant = 'primary'
+}) => {
 	return (
 		<button
 			className={clsx(
-				'px-[20px] py-[12px] bg-primary rounded-[12px] text-[16px] leading-[24px] text-white hover:bg-primaryHover active:bg-primaryPressed transition',
+				'px-[20px] py-[12px] rounded-[12px] text-[16px] leading-[24px] w-full ',
+				{
+					primary:
+						'bg-primary text-white hover:bg-primaryHover active:bg-primaryPressed border border-primary transition',
+					outline:
+						'bg-white text-textPrimary hover:bg-primaryHover hover:text-white active:bg-primaryPressed active:text-white border border-borderColor transition'
+				}[variant],
 				className
 			)}
 			onClick={onClick}
