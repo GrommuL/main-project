995a757a29fa3013151298f11869048f41f2eec6
Grommul <miketsvg@gmail.com>
2023-08-09 12:26:07 +0600

    feat: added changing the user's first name, last name and email

--- src/pages/Profile/ui/components/ProfileEditUserInformation.tsx
+++ src/pages/Profile/ui/components/ProfileEditUserInformation.tsx
@@ -1,17 +1,70 @@
 import { Button } from '@/components/ui/buttons'
-import { ProfileInput } from '@/components/ui/inputs'
-import { useAppSelector } from '@/utils/hooks/redux'
+import { AuthInput } from '@/components/ui/inputs'
+import { setUser } from '@/store/slices/authSlice'
+import { User } from '@/types/User'
+import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux'
+import { editProfileSchema } from '@/utils/schemas/editProfileSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios, { AxiosResponse } from 'axios'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 export const ProfileEditUserInformation = () => {
+	const dispatch = useAppDispatch()
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors }
+	} = useForm<User>({ resolver: zodResolver(editProfileSchema) })
 	const currentUser = useAppSelector((state) => state.auth?.user)
+
+	const onSubmit: SubmitHandler<User> = async (data) => {
+		try {
+			const response: AxiosResponse<User> = await axios.patch(
+				`http://localhost:8080/users/${String(currentUser?.id)}`,
+				data
+			)
+			dispatch(setUser(response.data))
+			reset()
+		} catch (error) {
+			console.log(error)
+		}
+	}
 	return (
 		<div className='flex flex-col gap-[24px] p-[32px]'>
 			<h4 className='text-[25px] font-semibold'>Личные данные</h4>
-			<form className='flex flex-col gap-[32px]'>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className='flex flex-col gap-[32px]'
+			>
 				<div className='flex items-center gap-[32px] w-full'>
-					<ProfileInput label='Имя' placeholder={currentUser?.firstName} />
-					<ProfileInput label='Фамилия' placeholder={currentUser?.lastName} />
-					<ProfileInput label='Email' placeholder={currentUser?.email} />
+					<AuthInput
+						id='firstName'
+						register={register}
+						error={errors.firstName}
+						label='Имя'
+						placeholder={currentUser?.firstName}
+						required
+						className='w-full'
+					/>
+					<AuthInput
+						id='lastName'
+						register={register}
+						error={errors.lastName}
+						label='Фамилия'
+						placeholder={currentUser?.lastName}
+						required
+						className='w-full'
+					/>
+					<AuthInput
+						id='email'
+						register={register}
+						error={errors.email}
+						label='Email'
+						placeholder={currentUser?.email}
+						required
+						className='w-full'
+					/>
 				</div>
 				<Button className='max-w-[128px]' label='Сохранить' type='submit' />
 			</form>
