10b046d0d132df8fb2570f0f5061bf3656a4397d
Grommul <miketsvg@gmail.com>
2023-08-10 13:22:14 +0600

    update: added receiving and displaying data of all users with the freelancer role

--- src/components/screens/best-freelancers-section/BestFreelancerSection.tsx
+++ src/components/screens/best-freelancers-section/BestFreelancerSection.tsx
@@ -1,6 +1,29 @@
+import { useEffect } from 'react'
 import { BestFreelancerPortfolioItem } from './ui'
+import { instance } from '@/utils/axios/instance'
+import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux'
+import { getFreelancers } from '@/store/slices/usersSlice'
+import axios, { AxiosResponse } from 'axios'
+import { User } from '@/types/User'
 
 export const BestFreelancerSection = () => {
+	const dispatch = useAppDispatch()
+	const users = useAppSelector((state) => state.users.users)
+	const getAllUsers = async () => {
+		try {
+			const response: AxiosResponse<User[]> = await axios.get(
+				'http://localhost:8080/users'
+			)
+			if (response.data) {
+				dispatch(getFreelancers({ users: [...response.data] }))
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	useEffect(() => {
+		void getAllUsers()
+	}, [])
 	return (
 		<section className='mb-[100px] pb-[32px]'>
 			<div className='container'>
@@ -8,15 +31,12 @@ export const BestFreelancerSection = () => {
 					<h2 className='font-bold text-[48px] leading-[120%]'>
 						Наши лучшие исполнители
 					</h2>
-					<div className='flex items-center justify-center gap-[32px] flex-wrap'>
-						<BestFreelancerPortfolioItem />
-						<BestFreelancerPortfolioItem />
-						<BestFreelancerPortfolioItem />
-						<BestFreelancerPortfolioItem />
-						<BestFreelancerPortfolioItem />
-						<BestFreelancerPortfolioItem />
-						<BestFreelancerPortfolioItem />
-						<BestFreelancerPortfolioItem />
+					<div className='flex items-center gap-[32px] flex-wrap'>
+						{users.map((user) => {
+							if (user.portfolio?.length) {
+								return <BestFreelancerPortfolioItem user={user} key={user.id} />
+							}
+						})}
 					</div>
 				</div>
 			</div>
