f3bcdb20d6c164da29b6bfc466e4570b9861c532
Grommul <miketsvg@gmail.com>
2023-08-14 13:33:25 +0600

    update: added tabs for orders

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,15 +1,49 @@
 import { LinkButton } from '@/components/ui/buttons'
+import { Tab } from '@/components/ui/tabs'
+import { useState } from 'react'
+
+type TaskCategoryTabs = 'all-tasks' | 'agreed-terms' | 'in-work' | 'closed'
 
 export const Orders = () => {
+	const [taskCategory, setTaskCategory] =
+		useState<TaskCategoryTabs>('all-tasks')
 	return (
 		<main className='pt-[50px] pb-[100px]'>
 			<div className='container'>
-				<div>
+				<div className='flex flex-col gap-[50px]'>
 					<div className='flex items-center justify-between px-[32px] py-[16px] bg-white rounded-[12px] shadow-dropdown'>
 						<span>Создайте заказ и выберите исполнителя</span>
 						<LinkButton href='/create-task' label='Создать' />
 					</div>
-					<div></div>
+					<div>
+						<div className='flex items-center gap-[24px]'>
+							<Tab
+								id='all-tasks'
+								category={taskCategory}
+								label='Все заказы'
+								onClick={() => setTaskCategory('all-tasks')}
+							/>
+							<Tab
+								id='agreed-terms'
+								category={taskCategory}
+								label='Согласование условий'
+								onClick={() => setTaskCategory('agreed-terms')}
+							/>
+							<Tab
+								id='in-work'
+								category={taskCategory}
+								label='В работе'
+								onClick={() => setTaskCategory('in-work')}
+							/>
+							<Tab
+								id='closed'
+								category={taskCategory}
+								label='Закрытые'
+								onClick={() => setTaskCategory('closed')}
+							/>
+						</div>
+						<div></div>
+					</div>
 				</div>
 			</div>
 		</main>
