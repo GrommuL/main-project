f012ac7fb7b76b1d357302d8cf6fbe3d5740cb9a
Grommul <miketsvg@gmail.com>
2023-08-14 13:51:07 +0600

    update: added top block with order information

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,4 +1,5 @@
 import { LinkButton } from '@/components/ui/buttons'
+import { SharedIcon, ViewIcon } from '@/components/ui/icons'
 import { Tab } from '@/components/ui/tabs'
 import { useState } from 'react'
 
@@ -42,7 +43,36 @@ export const Orders = () => {
 								onClick={() => setTaskCategory('closed')}
 							/>
 						</div>
-						<div></div>
+						<div className='p-[32px]'>
+							<div className='flex flex-col gap-[32px]'>
+								<div className='flex flex-col gap-[12px]'>
+									<div className='flex items-center justify-between'>
+										<div className='flex items-center gap-[16px]'>
+											<h3 className='text-[32px] font-bold leading-[38px]'>
+												Лендинг на Тильде
+											</h3>
+											<button
+												className='w-[44px] h-[44px] rounded-full bg-bgSecondary flex items-center justify-center border border-transparent hover:border-primaryHover transition'
+												onClick={() => {
+													console.log('click')
+												}}
+											>
+												<SharedIcon />
+											</button>
+											<div className='flex items-center gap-[4px]'>
+												<ViewIcon />
+												<span>150</span>
+											</div>
+										</div>
+										<span className='text-[32px] leading-[45px]'>
+											до 30 000 руб.
+										</span>
+									</div>
+									<div>Создано 03 октября 2022</div>
+								</div>
+								<div></div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
