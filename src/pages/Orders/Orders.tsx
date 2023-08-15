5bb3d11f226679d12fbb8034c043460c303e11c4
Grommul <miketsvg@gmail.com>
2023-08-15 14:29:24 +0600

    refactor: added TaskInformationBlock and TaskInformationSwitcher

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -3,9 +3,10 @@ import { Button, LinkButton } from '@/components/ui/buttons'
 import { SharedIcon, ViewIcon } from '@/components/ui/icons'
 import { Tab } from '@/components/ui/tabs'
 import { useState } from 'react'
+import { TaskInformationBlock, TaskInformationSwitcher } from './components'
 
 type TaskCategoryTabs = 'all-tasks' | 'agreed-terms' | 'in-work' | 'closed'
-type TaskInformationToggle = 'details' | 'responses'
+export type TaskInformationToggle = 'details' | 'responses'
 
 export const Orders = () => {
 	const [taskCategory, setTaskCategory] =
@@ -76,80 +77,11 @@ export const Orders = () => {
 									</div>
 									<div>Создано 03 октября 2022</div>
 								</div>
-								<div className='flex items-center'>
-									<Toggle
-										id='details'
-										label='Детали задания'
-										onClick={() => setTaskInformation('details')}
-										value='details'
-										name='taskDetails'
-										variant='left'
-										defaultChecked
-									/>
-									<Toggle
-										id='responses'
-										label='Отклики'
-										name='taskDetails'
-										onClick={() => setTaskInformation('responses')}
-										value='responses'
-										variant='right'
-										className={
-											taskInformation === 'responses'
-												? 'bg-primary text-white'
-												: ''
-										}
-									/>
-								</div>
-								<div className='flex items-end justify-between'>
-									{taskInformation === 'details' && (
-										<div className='flex flex-col gap-[16px]'>
-											<div>
-												<h5 className='text-[20px] leading-[26px] font-semibold'>
-													Номер задания
-												</h5>
-												<p>12345678</p>
-											</div>
-											<div>
-												<h5 className='text-[20px] leading-[26px] font-semibold'>
-													Категория и подкатегория
-												</h5>
-												<p>Дизайн - Веб дизайн</p>
-											</div>
-											<div>
-												<h5 className='text-[20px] leading-[26px] font-semibold'>
-													Описание услуги
-												</h5>
-												<p>
-													Сделать лендинг на Тильде для туроператора I LIKE
-													TRAVEL
-												</p>
-											</div>
-											<div>
-												<h5 className='text-[20px] leading-[26px] font-semibold'>
-													Какой бюджет
-												</h5>
-												<p>до 30 000 руб.</p>
-											</div>
-										</div>
-									)}
-									{taskInformation === 'responses' && (
-										<div className='flex flex-col gap-[16px]'>
-											<h5 className='text-[20px] leading-[26px] font-semibold'>
-												У задания пока нет откликов
-											</h5>
-											<p className='text-textSecondary max-w-[578px]'>
-												Скоро здесь появятся отклики исполнителей, готовых
-												выполнить ваше задание. Это может занять время.
-												Дождитесь, пока исполнители увидят задание и откликнутся
-												на него
-											</p>
-										</div>
-									)}
-									<Button
-										label='Отменить заказ'
-										className='bg-error text-white w-fit border border-error hover:bg-red-700 transition'
-									/>
-								</div>
+								<TaskInformationSwitcher
+									taskInformation={taskInformation}
+									setTaskInformation={setTaskInformation}
+								/>
+								<TaskInformationBlock taskInformation={taskInformation} />
 							</div>
 						</div>
 					</div>
