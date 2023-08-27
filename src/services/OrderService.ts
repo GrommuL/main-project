0751bb6b7b8e0cd67c5294515b5fbb3341fe229b
Grommul <miketsvg@gmail.com>
2023-08-27 17:42:43 +0600

    feat: added OrderService

---
+++ src/services/OrderService.ts
@@ -0,0 +1,24 @@
+import { getOrders } from '@/store/slices/ordersSlice'
+import { Order, Orders } from '@/types/OrderType'
+import { instance } from '@/utils/axios/instance'
+import { useAppDispatch } from '@/utils/hooks/redux'
+import { AxiosResponse } from 'axios'
+
+export const OrderService = () => {
+	const dispatch = useAppDispatch()
+	const getAllOrder = async () => {
+		const response: AxiosResponse<Order[]> = await instance.get(
+			'http://localhost:8080/tasks'
+		)
+		if (response.data) {
+			dispatch(
+				getOrders({
+					orders: response.data
+				})
+			)
+		}
+	}
+	return {
+		getAllOrder
+	}
+}
