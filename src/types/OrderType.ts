128610ab0c38098b4dce7b9f93f68ca4da57677e
Grommul <miketsvg@gmail.com>
2023-08-27 17:46:57 +0600

    update: changed Order types

--- src/types/OrderType.ts
+++ src/types/OrderType.ts
@@ -1,15 +1,20 @@
 import { User } from './User'
 
 export type Order = {
-	orderId: string | number
+	id?: number
+	orderTitle: string
 	orderCategory: string
 	orderPrice: string | number
 	orderAbout: string
 	createdAt: number
 	orderStatus: OrderStatus
-	responses: User[]
-	orderExecutor: User
-	orderOwner: User
+	responses: User[] | []
+	orderExecutor?: User
+	orderOwner: User | null
 }
 
 export type OrderStatus = 'active' | 'closed' | 'in-work'
+
+export type Orders = {
+	orders: Order[]
+}
